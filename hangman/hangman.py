f46d7bf4b3f6910a1b25051a528d32601cc20c00
Yekini Abdulwahab <104094403+F1re6ird@users.noreply.github.com>
2025-05-31 03:32:21 +0100

    one step for man, one giant leap for mankind

---
+++ hangman/hangman.py
@@ -0,0 +1,170 @@
+
+def play_hangman():
+    
+    import random
+
+    list_of_words = ["apple", "banana", "orange", "grape", "kiwi", "mango", "pineapple", "cherry", "watermelon", "strawberry"]
+
+    hangman = [
+        """
+        +---+
+        |   |
+        O   |
+    /|\\  |
+    / \\  |
+            |
+        =========
+        """,
+        """
+        +---+
+        |   |
+        O   |
+    /|\\  |
+    /    |
+            |
+        =========
+        """,
+        """
+        +---+
+        |   |
+        O   |
+    /|\\  |
+            |
+            |
+        =========
+        """,
+        """
+        +---+
+        |   |
+        O   |
+    /|   |
+            |
+            |
+        =========
+        """,
+        """
+        +---+
+        |   |
+        O   |
+        |   |
+            |
+            |
+        =========
+        """,
+        """
+        +---+
+        |   |
+        O   |
+            |
+            |
+            |
+        =========
+        """,
+        """
+        +---+
+        |   |
+            |
+            |
+            |
+            |
+        =========
+        """,
+        """
+        +---+
+            |
+            |
+            |
+            |
+            |
+        =========
+        """
+    ]
+
+
+    victory = """
+        +---+
+            |
+            |
+        \\O/ |
+        |  |
+        / \\ |
+        =========
+    """
+
+
+    flag = True
+
+    while flag:
+        
+        word = random.choice(list_of_words)
+        
+        lives = 7
+        guessed_letters = []
+        selected = []
+        
+        print("Please input (y) to continue (n) to stop playing")
+        
+        play = input("Play game? ")
+        if play.lower() == "y":
+            while lives > 0:
+                print(hangman[lives])
+                print()
+                print("Guessed letters:", ", ".join(guessed_letters))
+                print()
+
+                dummy_word = ""
+                for n in word:
+                    if n in selected:
+                        dummy_word += n
+                    else:
+                        dummy_word += "_"
+                    
+                        
+                if dummy_word == word:
+                    print("You've won!!!")
+                    print()
+                    print(victory)
+                    print()
+                    print(f"The word was: {word.upper()}")
+                    print("Game Over. Returning to main menu.\n")
+                    break
+                print("Word:", " ".join(dummy_word).upper())
+
+                print()
+                
+                guess = input(f"guess the letter: ")
+                if not guess.isalpha() or len(guess) != 1:
+                    print("! Please enter a single valid letter.")
+                    print()
+                    continue
+                
+                if guess in guessed_letters:
+                    print()
+                    print(f"{guess.capitalize()} is already a guessed letter")
+                    print()
+                    continue
+                
+                elif guess in word:
+                    if guess in selected:
+                        print(f"You already discovered '{guess}'. Try another letter.")
+                        continue
+                    selected.append(guess)
+                    guessed_letters.append(guess)
+                    continue
+                else:
+                    guessed_letters.append(guess)
+                    lives -= 1
+                    if lives == 0:
+                        print(hangman[lives])
+                        print()
+                        print(f"The word was: {word.upper()}")
+                        print("Game Over. Returning to main menu.")
+                        print()
+                        break
+        elif play.lower() == "n":
+            flag = False
+        else:
+            continue
+
+if __name__ == "__main__":
+    play_hangman()
